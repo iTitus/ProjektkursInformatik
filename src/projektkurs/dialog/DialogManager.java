53270f7eed6e448eba97d12138dfb815ac687506
iTitus <mil.vel@web.de>
2014-11-20 16:59:30 +0100

    Cleanup

--- src/projektkurs/dialog/DialogManager.java
+++ src/projektkurs/dialog/DialogManager.java
@@ -1,13 +1,13 @@
 package projektkurs.dialog;
 
 public class DialogManager {
-	
+
 	private static int currValue;
-	
+
 	public static void changeValueBy(int dValue) {
 		currValue += dValue;
 	}
-	
+
 	public static int getCurrValue() {
 		return currValue;
 	}
