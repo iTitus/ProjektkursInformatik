eafd0ad3cb200ad9c72fb7767bfd494d480e7c33
iTitus <mil.vel@web.de>
2014-12-05 15:50:25 +0100

    Cleanup

--- BlueJ/projektkurs/lib/Scripts.java
+++ BlueJ/projektkurs/lib/Scripts.java
@@ -4,8 +4,6 @@ import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 import projektkurs.Main;
-import projektkurs.cutscene.CutSceneManager;
-import projektkurs.gui.GuiDialogChooser;
 import projektkurs.util.I18n;
 
 /**
@@ -14,26 +12,12 @@ import projektkurs.util.I18n;
 public final class Scripts {
 
     /**
-     * Startet CutScene No. 1 und ändert das Spielfeld.
-     */
-    public static void cutSceneOne() {
-        CutSceneManager.startCutScene(CutScenes.cutSceneOne);
-    }
-
-    /**
      * Verliert das Spiel.
      */
     public static void loose() {
         Main.pause();
         JOptionPane.showOptionDialog(null, I18n.getString("description.loose"), I18n.getString("description.loose"), 0, JOptionPane.ERROR_MESSAGE, new ImageIcon(Images.nuke), new Object[] { I18n.getString("button.exit") }, null);
         Main.exit();
-    }
-
-    /**
-     * Zeigt den Testdialog.
-     */
-    public static void testDialog() {
-        Main.openGui(new GuiDialogChooser(Dialoge.test, null));
     }
 
     /**
